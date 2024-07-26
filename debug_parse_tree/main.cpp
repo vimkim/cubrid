04d8737668a1e31bf891fd1f9c32ba4c34ec0635
vimkim <kimdhyungg@gmail.com>
2024-07-26 17:08:41 +0900

    feat(debug_parse_tree): initial commit

---
+++ debug_parse_tree/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include "parser.h"
+
+void printUsage()
+{
+    std::cout << "Usage (recommended): \n\n $ debug_parse_tree < a_random_sql_file.sql \n\n"
+              << "Or, use it directly with stdin. For example,\n\n"
+              << "$ debug_parse_tree \n"
+              << "> select * from code; \n\n-- press enter twice after to finish.\n";
+}
+
+auto depth = 0;
+
+auto prefunc(PARSER_CONTEXT *p, PT_NODE *tree, void *arg, int *continue_walk) -> PT_NODE *
+{
+    depth++;
+    std::string indent(depth * 4, ' ');
+    std::cout << indent << pt_show_node_type(tree);
+
+    if (tree->node_type == PT_NAME)
+    {
+        std::cout << " : " << tree->info.name.original << std::endl;
+    }
+    std::cout << '\n';
+
+    return tree;
+}
+
+auto postfunc(PARSER_CONTEXT *p, PT_NODE *tree, void *arg, int *continue_walk) -> PT_NODE *
+{
+    depth--;
+  return tree;
+}
+
+int main(int argc, const char *argv[])
+{
+    for (int i = 1; i < argc; ++i)
+    {
+        std::string arg = argv[i];
+        if (arg == "-h" || arg == "--help")
+        {
+            printUsage();
+            return 0;
+        }
+    }
+
+    auto parser = parser_create_parser();
+    lang_init();
+    lang_set_charset(INTL_CODESET_UTF8);
+    lang_set_charset_lang("en_US");
+
+    std::stringstream buffer;
+    std::string line;
+
+    while (std::getline(std::cin, line) && !line.empty())
+    {
+        buffer << line << "\n";
+    }
+
+    std::string statement = buffer.str();
+
+    auto node = parser_parse_string(parser, statement.c_str());
+
+    auto tree_text = parser_print_tree(parser, *node);
+    std::cout << tree_text << std::endl;
+
+    parser_walk_tree(parser, *node, prefunc, nullptr, postfunc, nullptr);
+
+    parser_free_parser(parser);
+
+    return 0;
+}
