61307443635058f4ab4c48fab25b653b4d5172d5
vimkim <kimdhyungg@gmail.com>
2024-07-29 12:55:13 +0900

    format main.cpp

--- contrib/debug_parse_tree/main.cpp
+++ contrib/debug_parse_tree/main.cpp
@@ -3,70 +3,63 @@
 
 void printUsage()
 {
-    std::cout << "Usage (recommended): \n\n $ debug_parse_tree < a_random_sql_file.sql \n\n"
-              << "Or, use it directly with stdin. For example,\n\n"
-              << "$ debug_parse_tree \n"
-              << "> select * from code; \n\n-- press enter twice after to finish.\n";
+  std::cout << "Usage (recommended): \n\n $ debug_parse_tree < a_random_sql_file.sql \n\n"
+	    << "Or, use it directly with stdin. For example,\n\n"
+	    << "$ debug_parse_tree \n"
+	    << "> select * from code; \n\n-- press enter twice after to finish.\n";
 }
 
 auto depth = 0;
 
-auto prefunc(PARSER_CONTEXT *p, PT_NODE *tree, void *arg, int *continue_walk) -> PT_NODE *
+auto prefunc (PARSER_CONTEXT *p, PT_NODE *tree, void *arg, int *continue_walk) -> PT_NODE *
 {
-    depth++;
-    std::string indent(depth * 4, ' ');
-    std::cout << indent << pt_show_node_type(tree);
+  depth++;
+  std::string indent (depth * 4, ' ');
+  std::cout << indent << pt_show_node_type (tree);
 
-    if (tree->node_type == PT_NAME)
-    {
-        std::cout << " : " << tree->info.name.original;
-    }
-    std::cout << '\n';
+  std::cout << " : " << parser_print_tree (p, tree) << '\n';
 
-    return tree;
-}
-
-auto postfunc(PARSER_CONTEXT *p, PT_NODE *tree, void *arg, int *continue_walk) -> PT_NODE *
-{
-    depth--;
   return tree;
 }
 
-int main(int argc, const char *argv[])
+auto postfunc (PARSER_CONTEXT *p, PT_NODE *tree, void *arg, int *continue_walk) -> PT_NODE *
 {
-    for (int i = 1; i < argc; ++i)
+  depth--;
+  return tree;
+}
+
+int main (int argc, const char *argv[])
+{
+  for (int i = 1; i < argc; ++i)
     {
-        std::string arg = argv[i];
-        if (arg == "-h" || arg == "--help")
-        {
-            printUsage();
-            return 0;
-        }
+      std::string arg = argv[i];
+      if (arg == "-h" || arg == "--help")
+	{
+	  printUsage();
+	  return 0;
+	}
     }
 
-    auto parser = parser_create_parser();
-    lang_init();
-    lang_set_charset(INTL_CODESET_UTF8);
-    lang_set_charset_lang("en_US");
+  auto parser = parser_create_parser();
+  lang_init();
+  lang_set_charset (INTL_CODESET_UTF8);
+  lang_set_charset_lang ("en_US");
 
-    std::stringstream buffer;
-    std::string line;
+  std::stringstream buffer;
+  std::string line;
 
-    while (std::getline(std::cin, line) && !line.empty())
+  while (std::getline (std::cin, line) && !line.empty())
     {
-        buffer << line << "\n";
+      buffer << line << "\n";
     }
 
-    std::string statement = buffer.str();
+  std::string statement = buffer.str();
 
-    auto node = parser_parse_string(parser, statement.c_str());
+  auto node = parser_parse_string (parser, statement.c_str());
 
-    auto tree_text = parser_print_tree(parser, *node);
-    std::cout << tree_text << std::endl;
+  parser_walk_tree (parser, *node, prefunc, nullptr, postfunc, nullptr);
 
-    parser_walk_tree(parser, *node, prefunc, nullptr, postfunc, nullptr);
+  parser_free_parser (parser);
 
-    parser_free_parser(parser);
-
-    return 0;
+  return 0;
 }
