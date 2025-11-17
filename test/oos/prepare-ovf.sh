06931028f0762d2acfbd13219f402db06a285db0
Daehyun Kim <kimdhyungg@gmail.com>
2025-11-17 14:38:30 +0900

    minor(test/oos): change location where load files are generated

--- test/oos/prepare-ovf.sh
+++ test/oos/prepare-ovf.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 DBNAME=testdb
+TESTDIR=$(dirname "$0")
+LOADFILE=$TESTDIR/load_oos_heap.sql
 
 # 1) SQL 스크립트 파일 만들기 (테이블 생성 포함)
-cat > load_oos.sql << 'EOF'
+cat > $LOADFILE << 'EOF'
 DROP TABLE IF EXISTS t_oos_ovf;
 CREATE TABLE IF NOT EXISTS t_oos_ovf (
     id   INT,
@@ -17,9 +19,9 @@ EOF
 #    txt2: 'B' 12000개(12KB 근처)
 for i in $(seq 1 10000); do
   printf "INSERT INTO t_oos_ovf (id, txt1, txt2) VALUES (%d, RPAD('A', 400, 'A'), RPAD('B', 12000, 'B'));\n" "$i"
-done >> load_oos.sql
+done >> $LOADFILE
 
 # 3) CUBRID csql로 실행
-csql.sh -i load_oos.sql
+csql.sh -i $LOADFILE
 
 
