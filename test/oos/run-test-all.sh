ffab8da309d984cf3638f061e73f827820c91538
Daehyun Kim <kimdhyungg@gmail.com>
2025-11-17 15:52:39 +0900

    feat(run-test-all): final report tool

---
+++ test/oos/run-test-all.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+
+# heap
+# test/oos/run-test.sh test/oos/run_heap_id_only_10.sql
+test/oos/run-test.sh test/oos/run_heap_id_only_20.sql
+
+# test/oos/run-test.sh test/oos/run_heap_id_txt1_10.sql
+test/oos/run-test.sh test/oos/run_heap_id_txt1_20.sql
+
+test/oos/run-test.sh test/oos/run_heap_txt2_10.sql
+# test/oos/run-test.sh test/oos/run_heap_txt2_20.sql
+
+test/oos/run-test.sh test/oos/run_heap_all_10.sql
+# test/oos/run-test.sh test/oos/run_heap_all_20.sql
+
+# ovf
+# test/oos/run-test.sh test/oos/run_ovf_id_only_10.sql
+test/oos/run-test.sh test/oos/run_ovf_id_only_20.sql
+
+# test/oos/run-test.sh test/oos/run_ovf_id_txt1_10.sql
+test/oos/run-test.sh test/oos/run_ovf_id_txt1_20.sql
+
+test/oos/run-test.sh test/oos/run_ovf_txt2_10.sql
+# test/oos/run-test.sh test/oos/run_ovf_txt2_20.sql
+
+test/oos/run-test.sh test/oos/run_ovf_all_10.sql
+# test/oos/run-test.sh test/oos/run_ovf_all_20.sql
