6b63c0c672d3cb394c26597ba61e71683162305f
Daehyun Kim <kimdhyungg@gmail.com>
2025-11-17 15:24:10 +0900

    feat(check-oos-server): check for string compression and count(*)

--- test/oos/check-oos-server.sh
+++ test/oos/check-oos-server.sh
@@ -2,10 +2,17 @@
 
 # Run the command and check for at least one matching line
 if cubrid spacedb testdb -s | rg 'pagesize 16\.0K' >/dev/null; then
-    echo "PASS: pagesize is 16.0K"
+  echo "PASS: pagesize is 16.0K"
 else
-    echo "FAIL: pagesize is not 16.0K"
-    exit 1
+  echo "FAIL: pagesize is not 16.0K, or server down"
+  exit 1
+fi
+
+if cubrid paramdump testdb | rg 'enable_string_compression=n' >/dev/null; then
+  echo "PASS: enable_string_compression is n"
+else
+  echo "FAIL: enable_string_compression is not n, or server down"
+  exit 1
 fi
 
 csql.sh -c 'select count(*) from t_oos_heap'
