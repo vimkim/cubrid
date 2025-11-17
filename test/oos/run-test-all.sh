4d6ed8d7c7c6f5f70084a443596fd11c0de6c7a6
Daehyun Kim <kimdhyungg@gmail.com>
2025-11-17 20:23:51 +0900

    fix(test-all): fix test-all after meeting

--- test/oos/run-test-all.sh
+++ test/oos/run-test-all.sh
@@ -2,27 +2,15 @@
 
 
 # heap
-# test/oos/run-test.sh test/oos/run_heap_id_only_10.sql
-test/oos/run-test.sh test/oos/run_heap_id_only_100.sql
+test/oos/run-test.sh test/oos/run_heap_id_only_200.sql
 
-# test/oos/run-test.sh test/oos/run_heap_id_txt1_10.sql
-test/oos/run-test.sh test/oos/run_heap_id_txt1_20.sql
-
-test/oos/run-test.sh test/oos/run_heap_txt2_10.sql
-# test/oos/run-test.sh test/oos/run_heap_txt2_20.sql
+test/oos/run-test.sh test/oos/run_heap_txt_10.sql
 
 test/oos/run-test.sh test/oos/run_heap_all_10.sql
-# test/oos/run-test.sh test/oos/run_heap_all_20.sql
 
 # ovf
-# test/oos/run-test.sh test/oos/run_ovf_id_only_10.sql
-test/oos/run-test.sh test/oos/run_ovf_id_only_100.sql
+test/oos/run-test.sh test/oos/run_ovf_id_only_200.sql
 
-# test/oos/run-test.sh test/oos/run_ovf_id_txt1_10.sql
-test/oos/run-test.sh test/oos/run_ovf_id_txt1_20.sql
-
-test/oos/run-test.sh test/oos/run_ovf_txt2_10.sql
-# test/oos/run-test.sh test/oos/run_ovf_txt2_20.sql
+test/oos/run-test.sh test/oos/run_ovf_txt_10.sql
 
 test/oos/run-test.sh test/oos/run_ovf_all_10.sql
-# test/oos/run-test.sh test/oos/run_ovf_all_20.sql
