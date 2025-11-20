062fabe52cadfa9ad599dcf2c443b8a49d4b8108
Daehyun Kim <kimdhyungg@gmail.com>
2025-11-20 14:05:56 +0900

    fix(data prepare): allow settings row numbers when loading

--- test/oos/prepare-all-datasets.sh
+++ test/oos/prepare-all-datasets.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
+ROWS=$1
+
+if [ -z "$ROWS" ]; then
+  echo "Usage: $0 <number_of_rows>"
+  exit 1
+fi
+
 # Resolve the directory of this script
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
 # Fix the working directory to the script’s location
 cd "$SCRIPT_DIR" || return
 
-echo "Running prepare-heap.sh..."
-time ./prepare-heap.sh 1000000
+echo "Running prepare-heap.sh... $ROWS rows"
+time ./prepare-heap.sh "${ROWS}"
 echo
 
 echo "Running prepare-ovf.sh..."
-time ./prepare-ovf.sh 1000000
+time ./prepare-ovf.sh "${ROWS}"
 
--- test/oos/run-test-all.sh
+++ test/oos/run-test-all.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
+pwd
 
 # heap
-test/oos/run-test.sh test/oos/run_heap_id_only_200.sql
+test/oos/run-test.sh test/oos/run_heap_id_only_1.sql
 
-test/oos/run-test.sh test/oos/run_heap_txt_10.sql
-
-test/oos/run-test.sh test/oos/run_heap_all_10.sql
+test/oos/run-test.sh test/oos/run_ovf_id_only_1.sql
 
 # ovf
-test/oos/run-test.sh test/oos/run_ovf_id_only_200.sql
+test/oos/run-test.sh test/oos/run_heap_all_1.sql
 
-test/oos/run-test.sh test/oos/run_ovf_txt_10.sql
+test/oos/run-test.sh test/oos/run_ovf_all_1.sql
 
-test/oos/run-test.sh test/oos/run_ovf_all_10.sql
