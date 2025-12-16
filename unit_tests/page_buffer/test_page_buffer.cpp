b21e60898217f72cc342136ddadff233a66efe8c
Daehyun Kim <kimdhyungg@gmail.com>
2025-12-16 17:08:26 +0900

    basic pgbuf tests

--- unit_tests/page_buffer/test_page_buffer.cpp
+++ unit_tests/page_buffer/test_page_buffer.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
-#include "page_buffer.h"
+// #include "page_buffer.h"
 #include "test_page_buffer_common.hpp"
+#include "page_buffer.c"
 
 // Demonstrate some basic assertions.
 TEST (HelloTest, BasicAssertions)
@@ -11,9 +12,64 @@ TEST (HelloTest, BasicAssertions)
   EXPECT_EQ (7 * 6, 42);
 }
 
-TEST (PageBufferTest, SampleTest)
+TEST (PageBufferTest, InitFinalize)
 {
+  // int rc = pgbuf_initialize();
+  // EXPECT_EQ (rc, NO_ERROR);
+  //
+  // pgbuf_finalize();
+}
 
+TEST (PageBufferTest, HashVpid)
+{
+  VPID vpid1 = { 1, 100 };
+  VPID vpid2 = { 2, 200 };
+  unsigned int htsize = 1024;
+
+  unsigned int hash1 = pgbuf_hash_vpid (&vpid1, htsize);
+  unsigned int hash2 = pgbuf_hash_vpid (&vpid2, htsize);
+
+  EXPECT_NE (hash1, hash2);
+}
+
+TEST (PageBufferTest, CompareVpid)
+{
+  VPID vpid1 = { 1, 100 };
+  VPID vpid2 = { 1, 100 };
+  VPID vpid3 = { 2, 200 };
+
+  int cmp1 = pgbuf_compare_vpid (&vpid1, &vpid2);
+  int cmp2 = pgbuf_compare_vpid (&vpid1, &vpid3);
+
+  EXPECT_EQ (cmp1, 0);
+  EXPECT_NE (cmp2, 0);
+}
+
+TEST (PageBufferTest, IsValidPage)
+{
+  bool is_valid = pgbuf_is_valid_page (thread_p, &vpid_Null_vpid, true);
+
+  EXPECT_FALSE (is_valid);
+}
+
+
+TEST (PageBufferTest, FixDebugOldPage)
+{
+  // auto page_ptr = pgbuf_fix (thread_p, &vpid_Null_vpid, PAGE_FETCH_MODE::OLD_PAGE, PGBUF_LATCH_MODE::PGBUF_LATCH_READ,
+  // 	     PGBUF_LATCH_CONDITION::PGBUF_UNCONDITIONAL_LATCH);
+}
+
+TEST (PageBufferTest, FixNewPage)
+{
+  // auto page_ptr = pgbuf_fix (thread_p, &vpid_Null_vpid, PAGE_FETCH_MODE::NEW_PAGE, PGBUF_LATCH_MODE::PGBUF_LATCH_READ,
+  // 	     PGBUF_LATCH_CONDITION::PGBUF_UNCONDITIONAL_LATCH);
+}
+
+TEST (PageBufferTest, FixOldPageIfInBuffer)
+{
+  auto page_ptr = pgbuf_fix (thread_p, &vpid_Null_vpid, PAGE_FETCH_MODE::OLD_PAGE_IF_IN_BUFFER,
+  	     PGBUF_LATCH_MODE::PGBUF_LATCH_READ, PGBUF_LATCH_CONDITION::PGBUF_UNCONDITIONAL_LATCH);
+  EXPECT_EQ (page_ptr, nullptr);
 }
 
 int main (int argc, char **argv)
@@ -22,11 +78,6 @@ int main (int argc, char **argv)
   ::testing::AddGlobalTestEnvironment (new ServerEnv());
   ::testing::GTEST_FLAG (break_on_failure) = true;
 
-  // TIP:
-  // While on active development, oos_log level is set to DEBUG.
-  // This makes the test output verbose.
-  // We need to explicitly set it to INFO or higher level to make test output clean.
-  // For debugging test failures, we can set it back to DEBUG or TRACE.
   return RUN_ALL_TESTS();
 }
 
